c0d4f7e8d449414670a47320e08e46c7b5372fcc
bustobri000 <bustobri000@warren.k12.in.us>
2022-02-07 13:22:56 -0500

    Probably done

--- src/org/WalkerCC_CompSci/Rectangle.java
+++ src/org/WalkerCC_CompSci/Rectangle.java
@@ -4,7 +4,6 @@ public class Rectangle {
     protected double length;
     protected double width;
 
-
     public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
